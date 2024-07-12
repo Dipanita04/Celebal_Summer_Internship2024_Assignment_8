024d48b821d58b4e021f998f7e98b862571e8686
Dipanita Mallick <138706267+Dipanita04@users.noreply.github.com>
2024-07-12 21:53:51 +0530

    Add files via upload

---
+++ Celebal Week 8 Assignment/assignment8/controllers/weatherController.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+const weatherController = async (req, res) => {
+  const city = req.query.city || "London";
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+
+  console.log("City:", city);
+  console.log("API Key:", apiKey);
+
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching weather data",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = weatherController;
